5a9d941d9c0ce3373588a87ab95aa33c90a8c740
Maksim Suvorov <suvormaks@yandex.ru>
2022-11-22 10:12:26 +0300

    fix remarks

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        PersonSurnameLengthComparator pcomp = new PersonSurnameLengthComparator(4); //Кто первый в списке тот более "знатен"
+        PersonSurnameLengthComparator pcomp = new PersonSurnameLengthComparator(6); //Кто первый в списке тот более "знатен"
         ArrayList<Person> people = new ArrayList<>();
         people.add(new Person("Victor", "Li", 25));
         people.add(new Person("Ivan", "Pi", 13));
         people.add(new Person("Olya", "Ews", 32));
         people.add(new Person("Kate", "Ivanova", 15));
         people.add(new Person("Petya", "Ivanov", 17));
+        people.add(new Person("Max", "Van Dayk", 75));
+        people.add(new Person("Vika", "Konstantinovna", 5));
         System.out.println(people);
         System.out.println();
         Collections.sort(people, pcomp);
